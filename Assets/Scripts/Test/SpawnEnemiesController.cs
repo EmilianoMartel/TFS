7105385f85e57a5b981b06e047aa9c8253de9cba
Emiliano <alohacocki@gmail.com>
2024-06-25 15:56:57 -0300

    Implemented animation riggin

--- Assets/Scripts/Test/SpawnEnemiesController.cs
+++ Assets/Scripts/Test/SpawnEnemiesController.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class SpawnEnemiesController : MonoBehaviour
 {
-    //[SerializeField] private List<Enemy> _enemyList = new List<Enemy>();
-    //
-    //public Enemy SpawnEnemy(Transform generatorPosition)
-    //{
-    //    Enemy enemy = Instantiate(_enemyList[0], transform.position, Quaternion.identity);
-    //    enemy.SetTarget(generatorPosition);
-    //    return enemy;
-    //}
+    [SerializeField] private List<Enemy> _enemyList = new List<Enemy>();
+    private List<EnemyType> _activeEnemies = new();
+    private List<EnemyType> _desactiveEnemies = new();
 
-    private void SelectEnemy()
+    struct EnemyType {
+        public string enemyType;
+        public List<Enemy> enemyList;
+    };
+
+    private void StartList()
     {
-
+        EnemyType temp;
+        for (int i = 0; i < _enemyList.Count; i++)
+        {
+            temp.enemyType = _enemyList[i].name;
+            temp.enemyList = new();
+            _activeEnemies.Add(temp);
+            _desactiveEnemies.Add(temp);
+        }
+        
     }
+
+    public Enemy SpawnEnemy(Transform generatorPosition)
+    {
+        Enemy enemy = Instantiate(_enemyList[0], transform.position, Quaternion.identity);
+        //enemy.SetTarget(generatorPosition);
+        return enemy;
+    }
+
+    //private Enemy SelectEnemy()
+    //{
+    //    int randomIndex = UnityEngine.Random.Range(0, _enemyList.Count);
+    //    if (_desactiveEnemies.Contains(_enemyList[randomIndex]))
+    //    {
+    //
+    //    }
+    //}
+    //
+    //private bool HaveDesactiveEnemy(List<Enemy> listEnemy,out Enemy enemy)
+    //{
+    //
+    //}
 }
